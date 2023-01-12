00b208f2993b9a74ca89a7fc610ba1fd7f140e1e
ALASTER NG JIE XIONG 2105004B <2105004B@student.tp.edu.sg>
2023-01-12 14:30:07 +0800

    Entity updated

--- Project/Shared/Domain/BaseDomainModel.cs
+++ Project/Shared/Domain/BaseDomainModel.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Project.Shared.Domain
 {
-    class BaseDomainModel
+    public abstract class BaseDomainModel
     {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
     }
+    public class Staff : BaseDomainModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int ContactNumber { get; set; }
+
+
+    }
+    public class EventMember: BaseDomainModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ContactNumber { get; set; }
+        public int DOB { get; set; }
+        public string Gender { get; set; }
+
+    }
+    public class Organizer : BaseDomainModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ContactNumber { get; set; }
+        public int DOB { get; set; }
+        public string Gender { get; set; }
+
+    }
+    public class Event : BaseDomainModel
+    {
+        public int OrganizerID { get; set; }
+        public int VenueID { get; set; }
+        public int StaffID { get; set; }
+        public string EventType { get; set; }
+        public DateTime Date { get; set; }
+        public virtual List<Venue> Venue { get; set; }
+    }
+    public class Registration:BaseDomainModel
+    {
+        public DateTime DateTimeRegistered { get; set; }
+        public int EventID { get; set; }
+        public int PaymentAmount { get; set; }
+        public string PaymentType { get; set; }
+
+        public string EventMemberID { get; set; }
+
+
+    }
+
+
+  
 }
---
+++ Project/Shared/Domain/Venue.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Project.Shared.Domain
+{
+    public class Venue
+    {
+        public string VenueType { get; set; }
+        public string Location { get; set; }
+    }
+}
